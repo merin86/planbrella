b4cf275c69d776a726fadc01e8106af55e5e75c9
merin86 <m_liljenroth@hotmail.com>
2024-05-15 13:14:29 +0000

    Added profile serializer and list view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+class ProfileList(APIView):
+    """
+    List all profiles. Profiles are created automatically when a user is registered.
+    """
+    def get(self, request, format=None):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
