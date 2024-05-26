d104941f2c69cb37ad90c645faf25cc4b56fe6ef
merin86 <m_liljenroth@hotmail.com>
2024-05-26 12:25:51 +0000

    Change ALLOWED_HOST

--- planbrella_api/settings.py
+++ planbrella_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -60,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['.gitpod.io', '.herokuapp.com', '127.0.0.1', 'planbrella.herokuapp.com']
+ALLOWED_HOSTS = ['.gitpod.io', '.herokuapp.com', '127.0.0.1', os.environ.get('ALLOWED_HOST')]
 
 # CSRF settings
 CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com', 'https://*.gitpod.io']
@@ -105,9 +106,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
