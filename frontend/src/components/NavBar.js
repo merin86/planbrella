8e75ab5f62b6d005ec50f5f62e3e88fc3862a1b1
merin86 <m_liljenroth@hotmail.com>
2024-05-27 19:05:54 +0000

    Style the navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/logo.jpg";
+import '../styles/NavBar.module.css';
 
 function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">Planbrella</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="logo" height="60" width="80" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">  {/* Marginal till vänster för att flytta innehållet till höger */}
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+          <Nav className="mx-auto text-center d-flex align-items-center">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#sign-in">Sign in</Nav.Link>
+            <Nav.Link href="#sign-up">Sign up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
