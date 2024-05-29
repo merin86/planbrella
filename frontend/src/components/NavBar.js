d6adc81cdd1b4a261c16ec2a3310baa5a27d5307
merin86 <m_liljenroth@hotmail.com>
2024-05-29 12:45:36 +0000

    Add style for navbar and import font from google

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.jpg";
-import '../styles/NavBar.module.css';
+import styles from '../styles/NavBar.module.css';
 
 function NavBar() {
   return (
-    <Navbar bg="light" expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand href="#home">
-          <img src={logo} alt="logo" height="60" width="80" />
+          <img src={logo} alt="logo" height="90" width="90" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
           <Nav className="mx-auto text-center d-flex align-items-center">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#sign-in">Sign in</Nav.Link>
-            <Nav.Link href="#sign-up">Sign up</Nav.Link>
+            <Nav.Link href="#home" className={styles.NavLink}>Home</Nav.Link>
+            <Nav.Link href="#sign-in" className={styles.NavLink}>Sign in</Nav.Link>
+            <Nav.Link href="#sign-up" className={styles.NavLink}>Sign up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
