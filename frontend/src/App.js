7f8ca79f58329484e71a569e810fbbb2e8d3998e
merin86 <m_liljenroth@hotmail.com>
2024-05-29 15:11:50 +0000

    Added components for links with routes from react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import styles from "./App.module.css";
+import { Route, Routes } from "react-router-dom";
+import classNames from 'classnames';
+import Home from './components/Home';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 import NavBar from "./components/NavBar";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className={styles.App}>
+    <div className={classNames(styles.App, styles.content)}>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="*" element={<p>Page not found!</p>} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,21 +1,49 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { NavLink, Link } from "react-router-dom";
 import logo from "../assets/logo.jpg";
-import styles from '../styles/NavBar.module.css';
+import styles from "../styles/NavBar.module.css";
 
 function NavBar() {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">
-          <img src={logo} alt="logo" height="90" width="90" />
+        <Navbar.Brand as={Link} to="/">
+          <img src={logo} alt="logo" height="80" width="80" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
-          <Nav className="mx-auto text-center d-flex align-items-center">
-            <Nav.Link href="#home" className={styles.NavLink}>Home</Nav.Link>
-            <Nav.Link href="#sign-in" className={styles.NavLink}>Sign in</Nav.Link>
-            <Nav.Link href="#sign-up" className={styles.NavLink}>Sign up</Nav.Link>
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="justify-content-center"
+        >
+          <Nav
+            className={`mx-auto text-center d-flex align-items-center ${styles.NavLinks}`}
+          >
+            <Nav.Link
+              as={NavLink}
+              to="/"
+              end
+              className={styles.NavLink}
+              activeClassName={styles.NavLinkActive}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              to="/sign-in"
+              className={styles.NavLink}
+              activeClassName={styles.NavLinkActive}
+            >
+              Sign in
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              to="/sign-up"
+              className={styles.NavLink}
+              activeClassName={styles.NavLinkActive}
+            >
+              Sign up
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
