6338075b909d1abab50c0f05f45aa90e1183c160
merin86 <m_liljenroth@hotmail.com>
2024-06-01 13:04:10 +0000

    Create and style footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import classNames from 'classnames';
+import classNames from "classnames";
 import NavBar from "./components/NavBar";
-import Home from './components/Home';
-import SignInForm from './pages/auth/SignInForm';
-import SignUpForm from './pages/auth/SignUpForm';
+import Home from "./components/Home";
+import SignInForm from "./pages/auth/SignInForm";
+import SignUpForm from "./pages/auth/SignUpForm";
+import Footer from "./components/Footer";
 import styles from "./App.module.css";
-import useMenuState from './hooks/useMenuState';
+import useMenuState from "./hooks/useMenuState";
 import "./api/axiosDefaults";
 
 function App() {
   const { menuOpen, setMenuOpen, paddingTop } = useMenuState();
 
   return (
-    <div className={classNames(styles.App, { [styles.contentShift]: menuOpen })}>
+    <div
+      className={classNames(styles.App, { [styles.contentShift]: menuOpen })}
+    >
       <NavBar onToggle={(isOpen) => setMenuOpen(isOpen)} />
-      <div className={styles.content} style={{ paddingTop }}>
+      <div className={styles.content} style={{ paddingTop, flex: 1 }}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sign-in" element={<SignInForm />} />
@@ -23,6 +26,7 @@ function App() {
           <Route path="*" element={<p>Page not found!</p>} />
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.Footer}>
+      <h3 className={styles.Title}>Contact Planbrella &copy;</h3>
+      <p className={styles.Paragraph}>Phone Number: 08-123 45 67</p>
+      <p className={styles.Paragraph}>E-mail: admin@planbrella.com</p>
+    </footer>
+  );
+};
+
+export default Footer;
